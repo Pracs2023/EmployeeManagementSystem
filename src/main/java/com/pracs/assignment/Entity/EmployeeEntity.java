869ac0be5a8e39c1cb4e6e152857dd32b521a6fc
Pracs2023 <pracs2023@gmail.com>
2024-06-07 19:25:42 +0530

    new

---
+++ src/main/java/com/pracs/assignment/Entity/EmployeeEntity.java
@@ -0,0 +1,17 @@
+package com.pracs.assignment.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+}
---
+++ src/main/java/com/pracs/assignment/Service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.pracs.assignment.Service;
+
+import com.pracs.assignment.Model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
+
